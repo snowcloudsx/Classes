3668f1858ce1e95818a48011c3dab54549ef26ef
Snow <snowcloudsdev@gmail.com>
2024-10-10 18:33:28 -0400

    MAin
    snhass

---
+++ script.js
@@ -0,0 +1,18 @@
+function StartWatch()
+{
+    //start watch
+    alert("Start")
+}
+
+function Stop_Watch()
+{
+    //stop watch
+    alert("Stop")
+}
+
+function ResetWatch() 
+{
+    //reset
+    alert("Reset")
+    
+}
